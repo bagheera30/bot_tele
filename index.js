56b48101523df4cb461d4a3af5f3f1be3d4318ee
bagheera30 <rizkialfian30103@gmail.com>
2023-01-17 00:05:53 +0700

    awal

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const token = "5921091811:AAENynl6Xpn7o972cxDPIjV2S194WXhfWlY";
 
 const bot = new TelegramBot(token, { polling: true });
 let helloWorldSent = false;
+let generate = false;
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
@@ -14,13 +15,17 @@ bot.on("message", (msg) => {
     bot.sendMessage(chatId, "Hello, world!");
     helloWorldSent = true;
   }
+  if (!generate) {
+    bot.onText(/\/generate (.+)/, (msg, match) => {
+      const chatId = msg.chat.id;
+      const passwordLength = match[1];
+      const password = randomstring.generate({
+        length: passwordLength,
+        charset: "alphanumeric",
+      });
+      bot.sendMessage(chatId, `Password Anda: ${password}`);
+    });
+    generate = true;
+  }
 });
-bot.onText(/\/generate (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const passwordLength = match[1];
-  const password = randomstring.generate({
-    length: passwordLength,
-    charset: "alphanumeric",
-  });
-  bot.sendMessage(chatId, `Password Anda: ${password}`);
-});
+``;
